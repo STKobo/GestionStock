57a98893da2791481f563ab792ce17d93336e1fa
Stephane_tcha <stephanetcha@gmail.com>
2023-02-13 23:39:46 +0100

    mise en place des classes AbstractEntity, Article, Category, Client

---
+++ src/main/java/com/example/GestionStock/model/Article.java
@@ -0,0 +1,42 @@
+package com.example.GestionStock.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+import java.math.BigDecimal;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "article")
+public class Article extends AbstractEntity{
+
+    @Column(name = "codearticle")
+    private String codeArticle;
+
+    @Column(name = "designation")
+    private String designation;
+
+    @Column(name = "prixunitaireht")
+    private BigDecimal prixUnitaireHt;
+
+    @Column(name = "tauxtva")
+    private BigDecimal tauxTva;
+
+    @Column(name =  "prixunitairettc")
+    private BigDecimal prixUnitaireTtc;
+
+    @Column(name = "photo")
+    private String photo;
+
+    @ManyToOne
+    @JoinColumn(name = "idcategory")
+    private Category category;
+}
