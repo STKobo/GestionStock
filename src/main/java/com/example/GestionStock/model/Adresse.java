dfb296a9c7bb68237bcf1eb97d2372a17629a70b
Stephane_tcha <stephanetcha@gmail.com>
2023-02-14 23:31:18 +0100

    classe Adresse

---
+++ src/main/java/com/example/GestionStock/model/Adresse.java
@@ -0,0 +1,37 @@
+package com.example.GestionStock.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class Adresse {
+
+    @Column(name = "adresse1")
+    private String adresse1;
+
+    @Column(name = "adresse2")
+    private String adresse2;
+
+    @Column(name = "ville")
+    private String ville;
+
+    @Column(name = "codepostale")
+    private String codePostale;
+
+    @Column(name = "pays")
+    private String pays;
+
+}
