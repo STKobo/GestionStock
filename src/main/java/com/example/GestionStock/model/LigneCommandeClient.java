530c893efc6e7cff43cdb36573fc93feb14826a5
Stephane_tcha <stephanetcha@gmail.com>
2023-02-14 23:12:33 +0100

    ajout des classes LigneCommandeClient, LigneCommandeFournisseur, LigneVente, MvtStk, Roles

---
+++ src/main/java/com/example/GestionStock/model/LigneCommandeClient.java
@@ -0,0 +1,17 @@
+package com.example.GestionStock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "lignecommandeclient")
+public class LigneCommandeClient {
+}
