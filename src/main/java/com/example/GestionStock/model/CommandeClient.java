4fd84c6c2515c022be2b9ad0994138f83e32e754
Stephane_tcha <stephanetcha@gmail.com>
2023-02-14 23:41:19 +0100

    CommandeClient

--- src/main/java/com/example/GestionStock/model/CommandeClient.java
+++ src/main/java/com/example/GestionStock/model/CommandeClient.java
@@ -1,11 +1,14 @@
 package com.example.GestionStock.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -14,4 +17,17 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "commandeclient")
 public class CommandeClient extends AbstractEntity{
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "datecommande")
+    private Instant dateCommande;
+
+    @ManyToOne
+    @JoinColumn(name = "idclient")
+    private Client client;
+
+    @OneToMany(mappedBy = "commandeClient")
+    private List<LigneCommandeClient> ligneCommandeClients;
 }
