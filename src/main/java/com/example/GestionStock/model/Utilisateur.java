876c3281215b5621f0d3df4ba9a1676f70c8096c
Stephane_tcha <stephanetcha@gmail.com>
2023-02-18 19:27:11 +0100

    Utilisateurs

--- src/main/java/com/example/GestionStock/model/Utilisateur.java
+++ src/main/java/com/example/GestionStock/model/Utilisateur.java
@@ -1,11 +1,13 @@
 package com.example.GestionStock.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -15,5 +17,32 @@ import lombok.NoArgsConstructor;
 @Table(name = "utilisateur")
 public class Utilisateur extends AbstractEntity{
 
+    @Column(name = "nom")
     private String nom;
+
+    @Column(name = "prenom")
+    private String prenom;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "datedenaissance")
+    private Instant dateDeNaissance;
+
+    @Column(name = "motdepasse")
+    private String motDePasse;
+
+    @Embedded
+    private Adresse adresse;
+
+    @Column(name = "photo")
+    private String photo;
+
+    @ManyToOne
+    @JoinColumn(name = "identreprise")
+    private Entreprise entreprise;
+
+    @OneToMany(mappedBy = "utilisateur")
+    private List<Roles> roles;
+    
 }
