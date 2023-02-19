34c5fc57aece95e2eda899343bd5dfa4769c5570
Stephane_tcha <stephanetcha@gmail.com>
2023-02-19 23:45:24 +0100

    fromEntity() toEntity() AdresseDto

--- src/main/java/com/example/GestionStock/dto/AdresseDto.java
+++ src/main/java/com/example/GestionStock/dto/AdresseDto.java
@@ -1,6 +1,7 @@
 package com.example.GestionStock.dto;
 
 
+import com.example.GestionStock.model.Adresse;
 import lombok.Builder;
 import lombok.Data;
 
@@ -17,4 +18,31 @@ public class AdresseDto {
     private String codePostale;
 
     private String pays;
+
+    public AdresseDto fromEntity(Adresse adresse){
+        if(adresse == null){
+            return null;
+        }
+        return AdresseDto.builder()
+                .adresse1(adresse.getAdresse1())
+                .adresse2(adresse.getAdresse2())
+                .codePostale(adresse.getCodePostale())
+                .ville(adresse.getVille())
+                .pays(adresse.getPays())
+                .build();
+    }
+
+    public Adresse toEntity(AdresseDto adresseDto){
+        if(adresseDto == null){
+            return null; 
+        }
+        Adresse adresse = new Adresse();
+        adresse.setAdresse1(adresseDto.getAdresse1());
+        adresse.setAdresse2(adresseDto.getAdresse2());
+        adresse.setCodePostale(adresseDto.getCodePostale());
+        adresse.setVille(adresseDto.getVille());
+        adresse.setPays(adresseDto.getPays());
+        return adresse;
+    }
+
 }
