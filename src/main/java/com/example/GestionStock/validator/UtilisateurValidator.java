d46f217f02edc10bc41b5c8e9f54339f775e9c57
Stephane_tcha <stephanetcha@gmail.com>
2023-02-27 23:57:00 +0100

    mise en place validator Utilisateur

---
+++ src/main/java/com/example/GestionStock/validator/UtilisateurValidator.java
@@ -0,0 +1,15 @@
+package com.example.GestionStock.validator;
+
+import com.example.GestionStock.dto.UtilisateurDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilisateurValidator {
+
+    public static List<String> validate(UtilisateurDto utilisateurDto){
+        List<String> errors = new ArrayList<>();
+
+        return errors;
+    }
+}
