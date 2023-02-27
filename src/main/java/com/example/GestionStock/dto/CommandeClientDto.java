e55c163fd2f10dc38f31d96da411193767138144
Stephane_tcha <stephanetcha@gmail.com>
2023-02-27 17:56:54 +0100

    CommandeClient dto)

--- src/main/java/com/example/GestionStock/dto/CommandeClientDto.java
+++ src/main/java/com/example/GestionStock/dto/CommandeClientDto.java
@@ -1,6 +1,7 @@
 package com.example.GestionStock.dto;
 
 
+import com.example.GestionStock.model.CommandeClient;
 import lombok.Builder;
 import lombok.Data;
 
@@ -20,4 +21,28 @@ public class CommandeClientDto {
     private ClientDto client;
 
     private List<LigneCommandeClientDto> ligneCommandeClients;
+
+    public static CommandeClientDto fromEntity(CommandeClient commandeClients){
+        if(commandeClients == null){
+            return null;
+        }
+        return CommandeClientDto.builder()
+                .code(commandeClients.getCode())
+                .dateCommande(commandeClients.getDateCommande())
+                .client(commandeClients.getClient())
+                .ligneCommandeClients(commandeClients.getLigneCommandeClients())
+                .build();
+    }
+
+    public static CommandeClient toEntity(CommandeClientDto commandeClientDto){
+        if(commandeClientDto == null){
+
+        }
+        return CommandeClient.builder()
+                .code(commandeClientDto.getCode())
+                .dateCommande(commandeClientDto.getDateCommande())
+                .client(commandeClientDto.getClient())
+                .ligneCommandeClients(commandeClientDto.getLigneCommandeClients())
+                .build();
+    }
 }
